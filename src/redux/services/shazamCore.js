1f3dd6b40f9ef21524cf3a8f04605b9feaad4c68
Siddharth Patel <sidharthpatel012@gmail.com>
2023-07-18 00:56:23 -0400

    Create and Integrate ShazamCore Api service
    Create ShazamCore Api service that uses basic Redux logic with the appropriate headers
    Incorporate the ShazamCore service with the UI
    Perform error handling and page loading by rendering <Error> and <Loader> components respectively
    Traverse through the list of songs and display it using the SongCard component

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,27 @@
+/** Main File for making API Calls */
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+/**
+ * Creates an API service with Redux
+ */
+export const shazamCoreApi = createApi({
+	reducerPath: "shazamCoreApi",
+	baseQuery: fetchBaseQuery({
+		baseUrl: "https://shazam-core7.p.rapidapi.com/",
+		prepareHeaders: (headers) => {
+			headers.set(
+				"X-RapidAPI-Key",
+				"fc6c0d1124mshccfd87840c62139p1a00a3jsnf0b04d353752"
+			);
+			// headers.set("X-RapidAPI-Host", "shazam-core7.p.rapidapi.com");
+
+			return headers;
+		},
+	}),
+	endpoints: (builder) => ({
+		getTopCharts: builder.query({ query: () => "charts/get-top-songs-in-world" }),
+	}),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
